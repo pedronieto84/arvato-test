6715606a7cb6a739bc10fd330f30e224e9640774
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-01-14 13:59:44 +0100

    Ciudad Interface defined.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 
 import express, {Request, Response} from 'express'
+import {Ciudad}  from './intefaces'
 
 const app = express()
 
 app.get("/", (req: Request, res: Response):void => {
-        res.json({message:"hola"})
+    
+    const ciudadPrueba: Ciudad = {
+        nombre: 'Terrassa',
+        poblacion: 120000,
+        pais: "España",
+        region: "Cataluña"
+    }
+        res.json(ciudadPrueba)
 })
 
 app.listen("3000", ():void=> {
---
+++ src/intefaces.ts
@@ -0,0 +1,6 @@
+export interface Ciudad {
+    nombre: string
+    pais: string
+    poblacion: number
+    region?: string
+}
