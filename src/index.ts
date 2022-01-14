e8e422c4b26026a2f8104f9a7212db7c272121f4
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-01-14 13:56:36 +0100

    Express setup up and ready and type of express imported.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-console.log(2);
+
+import express, {Request, Response} from 'express'
+
+const app = express()
+
+app.get("/", (req: Request, res: Response):void => {
+        res.json({message:"hola"})
+})
+
+app.listen("3000", ():void=> {
+    console.log('server runing');
+})
  
